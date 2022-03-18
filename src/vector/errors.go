73bb03c5c67c3d721a0d0766b25a925eba17e272
miarao <miaraomer@gmail.com>
2022-03-18 16:35:29 +0200

    Added cross and dot product calc and enhanced functionalities

--- src/vector/errors.go
+++ src/vector/errors.go
@@ -4,4 +4,7 @@ import (
 	"errors"
 )
 
-var ErrIncompatibleDimensions = errors.New("incompatible vectors dimensions. different number of components")
+var (
+	ErrIncompatibleDimensions = errors.New("incompatible vectors dimensions. different number of components")
+	ErrNot3DVector            = errors.New("vector is not 3D")
+)
